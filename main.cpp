dfff5290ae124d520150461699185ee0a8916163
Victor Akinsowon <victor.akinsowon2@mail.dcu.ie>
2025-10-02 18:42:26 +0000

    Designing a basic class structure to generate weapon statistics for a video game.
    Making sure a very basic version works and thinking how I can implement OOP principles.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,49 @@
 
 using namespace std;
 
-int main(){
+class Weapon { //Parent class + thinking of making it an abstract class
+private: //Ideas of states. Liklely to be changed to protected
+    string rarity;
+    string material;
+    int damage;
+    int durability;
 
+public: //Ideas of Methods
+    Weapon(string rarity, string material, int damage, int durability)
+        : rarity(rarity), material(material), damage(damage), durability(durability) {
+            // Weapon created
+        }
+
+    void attack() {
+        // Attack or Damage
+    }
+
+    void repair() {
+        // Repair weapon
+    }
+
+    void displayInfo() {
+        cout << "Rarity: " << rarity << endl;
+        cout << "Material: " << material << endl;
+        cout << "Damage: " << damage << endl;
+        cout << "Durability: " << durability << endl;
+        cout << endl;
+    }
+
+    virtual ~Weapon(){
+        // Maybe message when weapon is destroyed
+    }; // destructor
+
+
+};
+
+int main() {
+    Weapon myWeapon("Epic", "Steel", 50, 100);
+    myWeapon.displayInfo();
+
+    Weapon *sword = new Weapon("Legendary", "Mythril", 100, 200);
+    sword->displayInfo();
+    
+    delete sword; 
+    return 0;
 }
