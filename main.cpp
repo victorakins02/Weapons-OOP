8b79a8484b4e64a625112aefbc54af3be3c3bfe4
Victor Akinsowon <victor.akinsowon2@mail.dcu.ie>
2025-10-06 12:57:24 +0000

    Update file main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 class Weapon { //Parent class + thinking of making it an abstract class
@@ -12,15 +11,15 @@ private: //Ideas of states. Liklely to be changed to protected
 public: //Ideas of Methods
     Weapon(string rarity, string material, int damage, int durability)
         : rarity(rarity), material(material), damage(damage), durability(durability) {
-            // Weapon created
+            // Weapon created/Constructor
         }
 
     void attack() {
-        // Attack or Damage
+        // Attack or Damage + may leave this abstract for child classes
     }
 
     void repair() {
-        // Repair weapon
+        // Repair weapon + may not do this
     }
 
     void displayInfo() {
@@ -38,11 +37,43 @@ public: //Ideas of Methods
 
 };
 
+class Sword : public Weapon {
+private:
+    int bladeLength;
+    int sharpness;
+    int bleedChance;
+public:
+    Sword(string rarity, string material, int damage, int durability, int bladeLength, int sharpness, int bleedChance)
+        : Weapon(rarity, material, damage, durability), bladeLength(bladeLength), sharpness(sharpness), bleedChance(bleedChance) {
+            // Sword created/Constructor
+        }
+
+    void slash() {
+        // Slash attack
+    }
+
+    void parry() {
+        // Parry attack
+    }
+
+    void displayInfo() {
+        Weapon::displayInfo(); // Call parent method to display common info
+        cout << "Blade Length: " << bladeLength << endl;
+        cout << "Sharpness: " << sharpness << endl;
+        cout << "Bleed Chance: " << bleedChance << "%" << endl;
+        cout << endl;
+    }
+
+    ~Sword(){
+        // Maybe message when sword is destroyed
+    }; // destructor
+};
+
 int main() {
     Weapon myWeapon("Epic", "Steel", 50, 100);
     myWeapon.displayInfo();
 
-    Weapon *sword = new Weapon("Legendary", "Mythril", 100, 200);
+    Sword *sword = new Sword("Legendary", "Mythril", 100, 200, 40, 90, 25); 
     sword->displayInfo();
     
     delete sword; 
